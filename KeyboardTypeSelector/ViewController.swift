9ae0386ca9d6fbe97f18fa8b30b762fcfc669548
cmyr <colin@cmyr.net>
2014-11-15 15:50:36 -0500

    Initial Commit

---
+++ KeyboardTypeSelector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardTypeSelector
+//
+//  Created by Colin Rofls on 2014-11-15.
+//  Copyright (c) 2014 whirlscape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
